0ff9f0bfb0c3b3c5cc768d7a8e0c26430cb386a3
Alden Peffer <alden.peffer@gmail.com>
2017-10-17 12:42:05 -0400

    Albums page working

--- src/app/content/album-lib-view/album-lib-view.component.ts
+++ src/app/content/album-lib-view/album-lib-view.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { View } from '../../view/view.model';
+import * as viewIds from '../../view/view.ids';
+import * as ActionGenerator from '../../view/view.actions';
+
+interface AppState{
+  view: View
+}
 
 @Component({
   selector: 'app-album-lib-view',
@@ -6,10 +16,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-lib-view.component.css']
 })
 export class AlbumLibViewComponent implements OnInit {
+  view: Observable<View>
+  viewIds = viewIds
 
-  constructor() { }
+  constructor(private store: Store<AppState>){
+    this.view = store.select('view');
+	}
 
   ngOnInit() {
+  }
+
+  loadAlbum(albumId){
+    this.store.dispatch(ActionGenerator.genLoadAlbum(albumId));
   }
 
 }
--- src/app/view/view.reducer.ts
+++ src/app/view/view.reducer.ts
@@ -4,19 +4,34 @@ import { View } from './view.model';
 import * as ActionTypes from './view.actions.types';
 
 /*FAKE LIBRARY -- DELETE LATER*/
-const fakeLibrary = [
-	{ id: 1, name: "Song A", artist: {id: 1, name: "Artist A"},
-	album: {id: 1, name: "Album A"}, runtime: "1:49"},
+const fakeLibrary = {
+	songs: [
+		{ id: 1, name: "Song A", artist: {id: 1, name: "Artist A"},
+		album: {id: 1, name: "Album A"}, runtime: "1:49"},
 
-	{ id: 2, name: "Hips Don't Lie", artist: {id: 3, name: "Shakira"},
-	album: {id: 7, name: "Cool Album Bro"}, runtime: "2:21"},
+		{ id: 2, name: "Hips Don't Lie", artist: {id: 3, name: "Shakira"},
+		album: {id: 7, name: "Cool Album Bro"}, runtime: "2:21"},
 
-	{ id: 3, name: "Bleed It Out", artist: {id: 14, name: "Creepy Dudes"},
-	album: {id: 19, name: "Test Album"}, runtime: "6:37"},
+		{ id: 3, name: "Bleed It Out", artist: {id: 14, name: "Creepy Dudes"},
+		album: {id: 19, name: "Test Album"}, runtime: "6:37"},
 
-	{ id: 4, name: "Prayer of the Refugee", artist: {id: 6, name: "Rise Against"},
-	album: {id: 3, name: "Generic Album Name"}, runtime: "5:43"},
-]
+		{ id: 4, name: "Prayer of the Refugee", artist: {id: 6, name: "Rise Against"},
+		album: {id: 3, name: "Generic Album Name LOOOOOONG"}, runtime: "5:43"},
+	],
+	albums: [
+		{id: 1, name: "Album A"},
+		{id: 7, name: "Cool Album Bro"},
+		{id: 19, name: "Test Album"},
+		{id: 3, name: "Generic Album Name LOOOOOONG"},
+	],
+	artists: [
+		{id: 1, name: "Artist A"},
+		{id: 3, name: "Shakira"}, 
+		{id: 14, name: "Creepy Dudes"},
+		{id: 6, name: "Rise Against"}
+	]
+
+}
 
 const defaultState: View  = {
 	viewId: viewIds.ARTIST_LIB,
@@ -38,6 +53,7 @@ export function viewReducer(state: View = defaultState, action) {
 
 		case ActionTypes.LOAD_LIBRARY:
 			//Get library data
+			//fakeLibrary = createLibrary(list of songs) -- will return {songs:[], albums: [], artists: []}
 			payload.library = fakeLibrary;
 			return newState(state, payload)
 			
